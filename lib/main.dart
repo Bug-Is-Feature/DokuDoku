49087dd73af3dc97d611f475e9a74c5cfcea197b
Katepiss Sriburapa <artcoding2544@gmail.com>
2023-01-01 12:49:13 +0700

    commit to pear try

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo  Pear try',
+      title: 'Flutter Demo pear',
       theme: ThemeData(
         // This is the theme of your application.
         //
