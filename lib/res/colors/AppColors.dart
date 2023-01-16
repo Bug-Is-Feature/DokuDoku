542df1c085afa29b6508e03f8e95959e05cb1b2c
Katepiss Sriburapa <artcoding2544@gmail.com>
2023-01-16 07:07:14 +0700

    add more color and build hourglass session input

--- lib/res/colors/AppColors.dart
+++ lib/res/colors/AppColors.dart
@@ -1,1 +1,50 @@
+import 'package:dokudoku/res/colors/BaseColors.dart';
+import 'package:flutter/material.dart';
 
+class AppColors implements BaseColors {
+  // Theme
+  @override
+  Color get colorDarkest => Color(0xff92603D);
+  @override
+  Color get colorDark => Color(0xffC78F68);
+  @override
+  Color get colorNormal => Color(0xffC79C7D);
+  @override
+  Color get colorNormal2 => Color(0xffE7C0A3);
+  @override
+  Color get colorLighter => Color(0xffF1D8C8);
+  @override
+  Color get colorLighter2 => Color(0xffFEE9CC);
+  @override
+  Color get colorLightest => Color(0xffFFF5E7);
+  @override
+  Color get colorWhite => Colors.white;
+
+  //decor color
+  @override
+  Color get pink => Color(0xffFF897D);
+  @override
+  Color get purple => Color(0xffAF82F8);
+  @override
+  Color get blue => Color(0xff67BFC7);
+
+  //sign
+  @override
+  Color get warning => Color(0xffF96161);
+  @override
+  Color get positive => Color(0xff76CC96);
+
+  //Gray Scale
+  @override
+  Color get greyDarker => Color(0xff6A6A6A);
+  @override
+  Color get greyDark => Color(0xff909090);
+  @override
+  Color get grey => Color(0xffCBCBCB);
+  @override
+  Color get greyLight => Color(0xffE2E2E2);
+  @override
+  Color get greyLighter => Color(0xffEDEDED);
+  @override
+  Color get greyLightest => Color(0xffF7F7F7);
+}
---
+++ lib/ui/components/hourglass_session_input.dart
@@ -0,0 +1,179 @@
+import 'package:dokudoku/ui/components/button.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:dokudoku/res/AppContextExtension.dart';
+
+class HourglassSessionInput extends StatefulWidget {
+  const HourglassSessionInput({super.key});
+
+  @override
+  State<HourglassSessionInput> createState() => _HourglassSessionInputState();
+}
+
+class _HourglassSessionInputState extends State<HourglassSessionInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: MediaQuery.of(context).size.height * 0.45,
+            decoration: BoxDecoration(
+              color: context.resources.color.colorNormal3,
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 50, 30, 40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    children: const [
+                      Text(
+                        'Session Duration',
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      Spacer(),
+                      SizedBox(
+                        width: 66,
+                        height: 35,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  width: 1,
+                                  color: Colors.white,
+                                ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  width: 1,
+                                  color: Colors.white,
+                                ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(),
+                              labelText: 'Minutes',
+                              labelStyle: TextStyle(
+                                  color: Color(0xff92603D), fontSize: 15),
+                              isDense: true),
+                        ),
+                      ),
+                      // Text('666666')
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: const [
+                      Text('Session', style: TextStyle(fontSize: 25)),
+                      Spacer(),
+                      SizedBox(
+                        width: 66,
+                        height: 35,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  width: 1,
+                                  color: Colors.white,
+                                ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  width: 1,
+                                  color: Colors.white,
+                                ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(),
+                              labelStyle: TextStyle(
+                                  color: Color(0xff92603D), fontSize: 15),
+                              labelText: 'Minutes',
+                              isDense: true),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: const [
+                      Text('Break time', style: TextStyle(fontSize: 25)),
+                      Spacer(),
+                      SizedBox(
+                        width: 66,
+                        height: 35,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  width: 1,
+                                  color: Colors.white,
+                                ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  width: 1,
+                                  color: Colors.white,
+                                ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(),
+                              labelText: 'Minutes',
+                              labelStyle: TextStyle(
+                                  color: Color(0xff92603D), fontSize: 15),
+                              isDense: true),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 40),
+                  Text(
+                    'Think again',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 40),
+                  Center(
+                    child: Button(
+                        child: Text('Confirm'),
+                        onPressed: () {},
+                        backgroundColor: context.resources.color.colorDark),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
