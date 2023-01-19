c62abaded912e459361cf328c77c4eac0b0d71b9
Katepiss Sriburapa <artcoding2544@gmail.com>
2023-01-19 11:48:37 +0700

    finish hourglass view and add  hourglass input

--- lib/ui/view/hourglass_view.dart
+++ lib/ui/view/hourglass_view.dart
@@ -1,5 +1,6 @@
 import 'package:dokudoku/ui/components/bottom_navbar.dart';
 import 'package:dokudoku/ui/components/custom_appbar.dart';
+import 'package:dokudoku/ui/components/hourglass_session_input.dart';
 import 'package:dokudoku/ui/components/navigation_drawer.dart';
 import 'package:dokudoku/ui/components/toggle_timer_button.dart';
 import 'package:flutter/foundation.dart';
@@ -22,13 +23,32 @@ class _HourglassViewState extends State<HourglassView> {
           child: Container(
               child: Column(
         children: [
-          ToggleTimerButton(),
-
-          // TimeController(),
           SizedBox(
-            height: 60,
+            height: MediaQuery.of(context).size.height * 0.1,
           ),
-          Text('Hourglass')
+          Container(
+            child: Text(
+              "24:59",
+              style: TextStyle(fontSize: 50),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            height: MediaQuery.of(context).size.height * 0.6,
+            decoration: BoxDecoration(
+                color: context.resources.color.colorWhite,
+                borderRadius: BorderRadius.circular(60),
+                boxShadow: [
+                  BoxShadow(
+                    color: context.resources.color.grey,
+                    blurRadius: 5,
+                    offset: Offset(0, 4),
+                  ),
+                ]),
+          )
         ],
       ))),
     );
