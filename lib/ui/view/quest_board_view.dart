3ac9ecfdc3fa6d801bd42d3d01323d40f7e851ce
Katepiss Sriburapa <artcoding2544@gmail.com>
2023-01-19 11:32:07 +0700

    first commit quest board view

---
+++ lib/ui/view/quest_board_view.dart
@@ -0,0 +1,35 @@
+import 'package:dokudoku/ui/components/quest_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:dokudoku/res/AppContextExtension.dart';
+
+class QuestBoardView extends StatefulWidget {
+  const QuestBoardView({super.key});
+
+  @override
+  State<QuestBoardView> createState() => _QuestBoardViewState();
+}
+
+class _QuestBoardViewState extends State<QuestBoardView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: context.resources.color.colorLightest,
+        body: Container(
+          child: Container(
+            color: context.resources.color.colorNormal3,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Column(
+                children: [
+                  QuestCard(),
+                  QuestCard(),
+                  QuestCard(),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
