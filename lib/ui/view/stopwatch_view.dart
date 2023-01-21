54b7dedbf6cc4988c1aee03c953a846297526e6b
Katepiss Sriburapa <artcoding2544@gmail.com>
2023-01-21 10:57:49 +0700

    delete comments

--- lib/ui/view/stopwatch_view.dart
+++ lib/ui/view/stopwatch_view.dart
@@ -21,97 +21,56 @@ class StopwatchView extends StatelessWidget {
       backgroundColor: context.resources.color.colorLightest,
       body: SingleChildScrollView(
         child: Center(
-            child: Container(
-                child: Column(
-          children: [
-            SizedBox(
-              height: 60,
-            ),
-            StopwatchTimer(),
-            SizedBox(
-              height: 70,
-            ),
-            Button(
-              child: provider.timerPlaying ? Text('Stop') : Text('Start'),
-              backgroundColor: provider.timerPlaying
-                  ? context.resources.color.colorDarkest
-                  : context.resources.color.colorDark,
+          child: Container(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                StopwatchTimer(),
+                SizedBox(
+                  height: 70,
+                ),
+                Button(
+                  child: provider.timerPlaying ? Text('Stop') : Text('Start'),
+                  backgroundColor: provider.timerPlaying
+                      ? context.resources.color.colorDarkest
+                      : context.resources.color.colorDark,
 
-              // provider.timerPlaying ? Text('Stop') : Text('Start'),
-              // backgroundColor: context.resources.color.warning,
-              onPressed: () {
-                if (provider.timerPlaying) {
-                  Provider.of<TimerService>(context, listen: false).stop();
-                  showDialog(
-                      context: context,
-                      builder: (context) => CustomDialog(
-                            title: "Reading result",
-                            description: provider.stopTime > 5
-                                ? (provider.stopTime <= 10
-                                    ? '${provider.stopTime.round().toString()} sec (saved)'
-                                    : '${provider.stopTime.round().toString()} min (saved)')
-                                : 'result not saved (less than 5 minutes)',
-                            buttonText: "Close",
-                          )
-                      // AlertDialog(
-                      //   alignment: Alignment.center,
-                      //   title: Text(
-                      //     "Reading result",
-                      //     style: TextStyle(
-                      //       fontSize: 30,
-                      //       fontWeight: FontWeight.bold,
-                      //       fontFamily: 'primary',
-                      //       color: context.resources.color.colorDarkest,
-                      //     ),
-                      //   ),
-                      //   content: Text(
-                      //     provider.stopTime > 5
-                      //         ? (provider.stopTime <= 10
-                      //             ? '${provider.stopTime.round().toString()} sec (saved)'
-                      //             : '${provider.stopTime.round().toString()} min (saved)')
-                      //         : 'result not saved (less than 5 minutes)',
-                      //     style: TextStyle(
-                      //       fontSize: 30,
-                      //       fontWeight: FontWeight.bold,
-                      //       fontFamily: 'primary',
-                      //       color: context.resources.color.colorDarkest,
-                      //     ),
-                      //   ),
-                      //   actions: [
-                      //     TextButton(
-                      //       onPressed: () => Navigator.pop(context),
-                      //       child: Text(
-                      //         'View Stat.',
-                      //         style: TextStyle(
-                      //           fontSize: 30,
-                      //           fontWeight: FontWeight.bold,
-                      //           fontFamily: 'primary',
-                      //           color: context.resources.color.colorDarkest,
-                      //         ),
-                      //       ),
-                      //     ),
-                      //     TextButton(
-                      //         onPressed: () => Navigator.pop(context),
-                      //         child: Text(
-                      //           'Close',
-                      //           style: TextStyle(
-                      //             fontSize: 30,
-                      //             fontWeight: FontWeight.bold,
-                      //             fontFamily: 'primary',
-                      //             color: context.resources.color.colorDarkest,
-                      //           ),
-                      //         )),
-                      //   ],
-                      // ),
+                  // provider.timerPlaying ? Text('Stop') : Text('Start'),
+                  // backgroundColor: context.resources.color.warning,
+                  onPressed: () {
+                    if (provider.timerPlaying) {
+                      Provider.of<TimerService>(context, listen: false).stop();
+                      showDialog(
+                        context: context,
+                        builder: (context) => CustomDialog(
+                          title: "Reading result",
+                          description: provider.stopTime > 5
+                              ? (provider.stopTime <= 10
+                                  ? '${provider.stopTime.round().toString()} sec (saved)'
+                                  : '${provider.stopTime.round().toString()} min (saved)')
+                              : 'result not saved (less than 5 minutes)',
+                          buttonText: "View Stat",
+                          buttonText2: "Close",
+                          numberOfButtons: 2,
+                          onPressed2: () {
+                            Navigator.pop(context);
+                          },
+                          onPressed: () {},
+                        ),
                       );
-                  Provider.of<TimerService>(context, listen: false).reset();
-                } else {
-                  Provider.of<TimerService>(context, listen: false).start();
-                }
-              },
-            )
-          ],
-        ))),
+                      Provider.of<TimerService>(context, listen: false).reset();
+                    } else {
+                      Provider.of<TimerService>(context, listen: false).start();
+                    }
+                  },
+                  size: Size(99, 44),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
