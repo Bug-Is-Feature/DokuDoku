42785202568ce66c7908f016be36a562a70e0697
Katepiss Sriburapa <artcoding2544@gmail.com>
2023-01-22 17:41:06 +0700

    separate themeData from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:dokudoku/provider/navigation_provider.dart';
 import 'package:dokudoku/res/AppContextExtension.dart';
+import 'package:dokudoku/theme_app.dart';
+import 'package:dokudoku/ui/view/auth_view.dart';
 import 'package:dokudoku/ui/view/home_view.dart';
 import 'package:dokudoku/services/timer_service.dart';
 import 'package:flutter/material.dart';
@@ -24,59 +26,12 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => NavigationProvider(),
       child: MaterialApp(
-        title: 'Dokudoku',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme(
-              primary: context.resources.color.colorDarkest,
-              onPrimary: context.resources.color.colorWhite,
-              brightness: Brightness.light,
-              secondary: context.resources.color.colorDark,
-              onSecondary: context.resources.color.colorWhite,
-              background: context.resources.color.colorLightest,
-              error: context.resources.color.warning,
-              onBackground: context.resources.color.colorDarkest,
-              onError: context.resources.color.colorWhite,
-              onSurface: context.resources.color.colorNormal2,
-              surface: context.resources.color.colorWhite),
-          textTheme: TextTheme(
-            headlineLarge: TextStyle(
-              fontSize: 50,
-              fontFamily: 'primary',
-              fontWeight: FontWeight.bold,
-              color: context.resources.color.colorDarkest,
-            ),
-            headline1: TextStyle(
-              fontSize: 30,
-              fontFamily: 'primary',
-              fontWeight: FontWeight.bold,
-              color: context.resources.color.colorDarkest,
-            ),
-            headline2: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'primary',
-                color: context.resources.color.colorDarkest),
-            subtitle1: TextStyle(
-                fontSize: 20,
-                fontFamily: 'primary',
-                color: context.resources.color.colorDarkest),
-            bodyText1: TextStyle(
-                fontSize: 14.0,
-                fontFamily: 'primary',
-                color: context.resources.color.colorDarkest),
-            bodyText2: TextStyle(
-                fontSize: 12.0,
-                fontFamily: 'primary',
-                color: context.resources.color.colorDarkest),
-            labelMedium: TextStyle(
-                fontSize: 10.0,
-                fontFamily: 'primary',
-                color: context.resources.color.colorDarkest),
+          title: 'Dokudoku',
+          debugShowCheckedModeBanner: false,
+          theme: themeData(context),
+          home: AuthView()
+          //HomeView(),
           ),
-        ),
-        home: HomeView(),
-      ),
     );
   }
 }
