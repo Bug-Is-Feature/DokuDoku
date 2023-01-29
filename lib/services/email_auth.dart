fe8aa6ba8d5fe370a0e2ed12d27460ec4f2758b7
Katepiss Sriburapa <artcoding2544@gmail.com>
2023-01-29 21:56:06 +0700

    custom auth snack bar

--- lib/services/email_auth.dart
+++ lib/services/email_auth.dart
@@ -1,9 +1,13 @@
+import 'package:dokudoku/services/user_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:dokudoku/res/AppContextExtension.dart';
+
 // import 'package:dokudoku/services/dokudoku_service/user_service.dart';
 
 class EmailPasswordAuth {
   const EmailPasswordAuth();
+  static String errorText = '';
 
   Future<void> signIn(BuildContext context, String email, String pwd) async {
     try {
@@ -14,23 +18,35 @@ class EmailPasswordAuth {
       ))
               .user;
 
-      //   await UserServices.updateLastLogin();
+      await UserServices.updateLastLogin();
 
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('${user!.email} signed in'),
+          backgroundColor: const Color(0xff76CC96),
+          content: Text(
+            '${user!.email} signed in',
+            style: const TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
         ),
       );
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
-          content: Text('Failed to sign in with email'),
+          backgroundColor: Color(0xffF96161),
+          content: Text(
+            "Failed to sign in with email",
+            style: TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
         ),
       );
     }
   }
 
-  Future<void> register(String email, String pwd) async {
+  Future<void> register(BuildContext context, String email, String pwd) async {
     try {
       UserCredential userCredential =
           await FirebaseAuth.instance.createUserWithEmailAndPassword(
@@ -38,12 +54,30 @@ class EmailPasswordAuth {
         password: pwd,
       );
 
-      //   await UserServices.createUser();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: const Color(0xff76CC96),
+          content: Text(
+            'Register successful',
+            style: const TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+        ),
+      );
+
+      await UserServices.createUser();
     } on FirebaseAuthException catch (e) {
+      print(e.message);
+      print(e.code);
+      errorText = e.code;
+
       if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
+        print(errorText);
       } else if (e.code == 'email-already-in-use') {
         print('The account already exists for that email.');
+      } else if (e.code == 'invalid-email') {
+        print('The email is invalid');
       }
     } catch (e) {
       print(e);
