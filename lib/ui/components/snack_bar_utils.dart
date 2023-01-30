3ded0afd83a0535678b1df30d594df072c0bff15
Nonbcc <starnonn35672@gmail.com>
2023-01-30 21:24:18 +0700

    add file

---
+++ lib/ui/components/snack_bar_utils.dart
@@ -0,0 +1,41 @@
+import 'package:dokudoku/res/AppContextExtension.dart';
+import 'package:flutter/material.dart';
+
+class SnackBarUtils {
+  static void showCustomSnackBar({
+    required BuildContext context,
+    required Color backgroundColor,
+    required String content,
+  }) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: backgroundColor,
+        content: Text(
+          content,
+          style: TextStyle(
+              color: context.resources.color.colorDarkest,
+              fontFamily: 'primary',
+              fontSize: 16),
+        ),
+      ),
+    );
+  }
+
+  static void showWarningSnackBar({
+    required BuildContext context,
+    required String content,
+  }) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: context.resources.color.warning,
+        content: Text(
+          content,
+          style: TextStyle(
+              color: context.resources.color.colorDarkest,
+              fontFamily: 'primary',
+              fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
