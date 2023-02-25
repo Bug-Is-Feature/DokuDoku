e510236d37563d113b103d97fda1226ecb8382cd
Nonbcc <77319796+Nonbcc@users.noreply.github.com>
2023-02-25 12:56:14 +0700

    change main logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,25 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:dokudoku/provider/navigation_provider.dart';
 import 'package:dokudoku/theme_app.dart';
-import 'package:dokudoku/services/timer_service.dart';
+import 'package:dokudoku/provider/book_provider.dart';
+import 'package:dokudoku/provider/timer_provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
 import 'package:dokudoku/firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:dokudoku/routes/router.gr.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await dotenv.load(fileName: '.env');
-  runApp(
-    ChangeNotifierProvider<TimerService>(
-      create: (_) => TimerService(),
-      child: MyApp(),
-    ),
-  );
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider<TimerProvider>(create: (context) => TimerProvider()),
+    ChangeNotifierProvider<BookProvider>(create: (context) => BookProvider()),
+  ], child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
