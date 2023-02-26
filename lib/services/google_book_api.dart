7daa5a6320475a581675c65820f1fc904ea8410f
Nonbcc <77319796+Nonbcc@users.noreply.github.com>
2023-02-26 14:20:28 +0700

    add printType to google book api

--- lib/services/google_book_api.dart
+++ lib/services/google_book_api.dart
@@ -6,7 +6,7 @@ class FetchGoogleBook {
   static Future<List<GoogleBook>> getBookList(String query) async {
     query = query.replaceAll(' ', '+');
     String fetchUrl =
-        'https://www.googleapis.com/books/v1/volumes?q=$query&maxResults=5';
+        'https://www.googleapis.com/books/v1/volumes?q=$query&printType=books&maxResults=5';
 
     final url = Uri.parse(fetchUrl);
     final response = await http.get(url);
