ff55301c562a5f18ad327a53fa8774371c1c8f85
Nonbcc <77319796+Nonbcc@users.noreply.github.com>
2023-03-26 11:27:51 +0700

    add level & exp logic

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,8 +1,13 @@
 import 'dart:convert';
+import 'package:dokudoku/model/user.dart';
+import 'package:dokudoku/provider/timer_provider.dart';
+import 'package:dokudoku/provider/user_provider.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:ntp/ntp.dart';
+import 'package:provider/provider.dart';
 
 Future<String> _formattedNTPWithTimezoneOffset() async {
   String twoDigits(int n) {
@@ -70,6 +75,52 @@ class UserServices {
 
     if (response.statusCode == 200) {
       print('Update user last login successfully');
+    } else {
+      print('API_ERROR: ${response.statusCode}');
+    }
+  }
+
+  static Future<Users> getUser() async {
+    final currentUser = FirebaseAuth.instance.currentUser!;
+    String idToken = await currentUser.getIdToken();
+
+    final response = await http.get(
+      Uri.parse(
+          "http://${dotenv.env['BACKEND_PATH']}/api/users/${currentUser.uid}/"),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $idToken'
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return Users.fromJson(data);
+    } else {
+      print('API_ERROR: ${response.statusCode}');
+      throw Exception('Failed to get user');
+    }
+  }
+
+  static Future<void> updateExp(BuildContext context, int exp) async {
+    final currentUser = FirebaseAuth.instance.currentUser!;
+    String idToken = await currentUser.getIdToken();
+    final provider = Provider.of<TimerProvider>(context, listen: false);
+
+    final response = await http.patch(
+      Uri.parse(
+          "http://${dotenv.env['BACKEND_PATH']}/api/users/${currentUser.uid}/"),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $idToken'
+      },
+      body: jsonEncode({
+        'current_exp': exp,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      print('Update user exp successfully');
     } else {
       print('API_ERROR: ${response.statusCode}');
     }
