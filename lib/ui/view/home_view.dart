1db517067f1862eb4dfc7e1453b5dfcffe7fd640
Katepiss Sriburapa <artcoding2544@gmail.com>
2023-04-02 17:40:09 +0700

    feat: add bottomNavbar_provider

--- lib/ui/view/home_view.dart
+++ lib/ui/view/home_view.dart
@@ -1,9 +1,11 @@
+import 'package:dokudoku/provider/bottomNavbar_provider.dart';
 import 'package:dokudoku/ui/components/bottom_navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:dokudoku/res/AppContextExtension.dart';
 import 'package:dokudoku/services/auth_service.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:dokudoku/routes/router.gr.dart';
+import 'package:provider/provider.dart';
 
 class HomeView extends StatefulWidget {
 //   final int id;
@@ -21,42 +23,72 @@ class _HomeViewState extends State<HomeView> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       body: AutoTabsScaffold(
-        extendBodyBehindAppBar: true,
-        // appBarBuilder: (context, tabsRouter) => AppBar(
-        //   elevation: 0,
-        //   backgroundColor: Colors.transparent,
-        //   centerTitle: true
-        //   leading: const AutoBackButton(),
-        //   actions: [
-        //     IconButton(
-        //       icon: const Icon(Icons.logout),
-        //       color: context.resources.color.colorDarkest,
-        //       onPressed: () async {
-        //         await AuthService.googleAuth.signOut();
-        //         await AuthService.signOut();
-        //         context.router.replace(const AuthRoute());
-        //       },
-        //     ),
-        //   ],
-        // ),
-        routes: const [
-          BookShelvesRoute(),
-          QuestBoardRoute(),
-          ProfileRoute(),
-        ],
-        bottomNavigationBuilder: (context, tabsRouter) => BottomNavigationBar(
-          backgroundColor: context.resources.color.colorDarkest,
-          items: BottomNavBar.listItemBottomBar,
-          currentIndex: tabsRouter.activeIndex,
-          onTap: tabsRouter.setActiveIndex,
-          elevation: BottomNavBar.elevation,
-          showSelectedLabels: BottomNavBar.showSelectedLabels,
-          showUnselectedLabels: BottomNavBar.showUnselectedLabels,
-          type: BottomNavBar.bottomNavigationBarType,
-          selectedItemColor: BottomNavBar.selectedItemColor,
-          unselectedItemColor: BottomNavBar.unSelectedItemColor,
-        ),
-      ),
+          extendBodyBehindAppBar: true,
+          routes: const [
+            BookShelvesRoute(),
+            BadgeRoute(),
+            ProfileRoute(),
+          ],
+          bottomNavigationBuilder: ((context, tabsRouter) {
+            return Consumer<BottomNavbarProvider>(
+              builder: (context, BottomNavbarProvider, _) {
+                return BottomNavigationBar(
+                  backgroundColor: context.resources.color.colorDarkest,
+                  items: BottomNavBar.listItemBottomBar,
+                  currentIndex: BottomNavbarProvider.currentIndex,
+                  onTap: (index) {
+                    BottomNavbarProvider.setIndex(index);
+                    tabsRouter.setActiveIndex(index);
+                    print(
+                        "bottom navbar index : ${BottomNavbarProvider.currentIndex}");
+                  },
+                  elevation: BottomNavBar.elevation,
+                  showSelectedLabels: BottomNavBar.showSelectedLabels,
+                  showUnselectedLabels: BottomNavBar.showUnselectedLabels,
+                  type: BottomNavBar.bottomNavigationBarType,
+                  selectedItemColor: BottomNavBar.selectedItemColor,
+                  unselectedItemColor: BottomNavBar.unSelectedItemColor,
+                );
+              },
+            );
+          })
+
+          // BottomNavigationBar(
+          //   backgroundColor: context.resources.color.colorDarkest,
+          //   items: BottomNavBar.listItemBottomBar,
+          //   currentIndex: tabsRouter.activeIndex,
+          //   onTap: tabsRouter.setActiveIndex,
+          //   elevation: BottomNavBar.elevation,
+          //   showSelectedLabels: BottomNavBar.showSelectedLabels,
+          //   showUnselectedLabels: BottomNavBar.showUnselectedLabels,
+          //   type: BottomNavBar.bottomNavigationBarType,
+          //   selectedItemColor: BottomNavBar.selectedItemColor,
+          //   unselectedItemColor: BottomNavBar.unSelectedItemColor,
+          // ),
+          ),
     );
+    // return Scaffold(
+    //   extendBodyBehindAppBar: true,
+    //   body: AutoTabsScaffold(
+    //     extendBodyBehindAppBar: true,
+    //     routes: const [
+    //       BookShelvesRoute(),
+    //       QuestBoardRoute(),
+    //       ProfileRoute(),
+    //     ],
+    //     bottomNavigationBuilder: (context, tabsRouter) => BottomNavigationBar(
+    //       backgroundColor: context.resources.color.colorDarkest,
+    //       items: BottomNavBar.listItemBottomBar,
+    //       currentIndex: tabsRouter.activeIndex,
+    //       onTap: tabsRouter.setActiveIndex,
+    //       elevation: BottomNavBar.elevation,
+    //       showSelectedLabels: BottomNavBar.showSelectedLabels,
+    //       showUnselectedLabels: BottomNavBar.showUnselectedLabels,
+    //       type: BottomNavBar.bottomNavigationBarType,
+    //       selectedItemColor: BottomNavBar.selectedItemColor,
+    //       unselectedItemColor: BottomNavBar.unSelectedItemColor,
+    //     ),
+    //   ),
+    // );
   }
 }
